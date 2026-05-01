cad8f7eba84e48558efbbffb6d85b11549c22962
Matheus Lopez <mah.dejesus@gmail.com>
2026-05-01 07:59:00 -0300

    stop

--- services/cronScheduleStopStartServicesRailway/railway-stop.js
+++ services/cronScheduleStopStartServicesRailway/railway-stop.js
@@ -1,8 +1,9 @@
 const RAILWAY_TOKEN = process.env.RAILWAY_TOKEN;
 const SERVICE_IDS = process.env.SERVICE_IDS;
+const ENVIRONMENT_ID = process.env.ENVIRONMENT_ID;
 
-if (!RAILWAY_TOKEN || !SERVICE_IDS) {
-  console.error("Faltando RAILWAY_TOKEN ou SERVICE_IDS");
+if (!RAILWAY_TOKEN || !SERVICE_IDS || !ENVIRONMENT_ID) {
+  console.error("Faltando RAILWAY_TOKEN, SERVICE_IDS ou ENVIRONMENT_ID");
   process.exit(1);
 }
 
@@ -10,12 +11,16 @@ const serviceIds = SERVICE_IDS.split(",").map((id) => id.trim());
 const GRAPHQL_ENDPOINT = "https://backboard.railway.app/graphql/v2";
 
 const query = `
-  mutation UpdateService($serviceId: String!, $replicas: Int!) {
-    serviceInstanceUpdate(serviceId: $serviceId, input: { numReplicas: $replicas })
+  mutation UpdateService($serviceId: String!, $environmentId: String!, $replicas: Int!) {
+    serviceInstanceUpdate(
+      serviceId: $serviceId,
+      environmentId: $environmentId,
+      input: { numReplicas: $replicas }
+    )
   }
 `;
 
-async function updateService(serviceId, replicas) {
+async function updateService(serviceId, environmentId, replicas) {
   const res = await fetch(GRAPHQL_ENDPOINT, {
     method: "POST",
     headers: {
@@ -24,7 +29,7 @@ async function updateService(serviceId, replicas) {
     },
     body: JSON.stringify({
       query,
-      variables: { serviceId, replicas },
+      variables: { serviceId, environmentId, replicas },
     }),
   });
 
@@ -42,7 +47,7 @@ async function main() {
 
   for (const serviceId of serviceIds) {
     try {
-      await updateService(serviceId, 0);
+      await updateService(serviceId, ENVIRONMENT_ID, 0);
       console.log(`✔ Serviço ${serviceId} desligado com sucesso`);
     } catch (err) {
       console.error(`✘ Falha ao desligar serviço ${serviceId}:`, err.message);
